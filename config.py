0275958f38d8c2736c380c0c32511b6c7daaeff6
Elango sakthivel <elangosakthivel0@gmail.com>
2026-01-29 15:49:51 +0530

    Add configuration class with database and JWT settings

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+    SECRET_KEY = "secret-key-change-this"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = "jwt-secret-key"
